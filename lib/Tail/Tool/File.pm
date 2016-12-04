9d746503874cc8f96c33be1c60b567b4b344b74b
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-12-05 06:52:55 +1100

    Updating for release of version 0.4.5

--- lib/Tail/Tool/File.pm
+++ lib/Tail/Tool/File.pm
@@ -14,7 +14,7 @@ use Scalar::Util qw/openhandle/;
 use English qw/ -no_match_vars /;
 use AnyEvent;
 
-our $VERSION = version->new('0.4.4');
+our $VERSION = version->new('0.4.5');
 
 has name => (
     is            => 'rw',
@@ -272,7 +272,7 @@ Tail::Tool::File - Looks after individual files
 
 =head1 VERSION
 
-This documentation refers to Tail::Tool::File version 0.4.4.
+This documentation refers to Tail::Tool::File version 0.4.5.
 
 =head1 SYNOPSIS
 
